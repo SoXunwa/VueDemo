5a584653d8ee2e8e1e705808c61d355d9d5cedb9
XuanhuaSu <suxuanhua@outlook.com>
2020-10-20 00:04:42 +0800

    webpack 学习

---
+++ src/Vue.js/19.vue文件中export用法.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{msg}}}</h1>
+</template>
+<style></style>
+<script>
+    export default {
+        //组件中的 data，必须时 function
+        data() {
+            return {
+                msg: "组件中的数据"
+            }
+        },
+        //组件中的方法
+        methods: {
+            show() {
+                console.log("调用了Vue组件文件的show()")
+            }
+        }
+    }
+</script>
